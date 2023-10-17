c8a51a2e0f057ac72b523a9ae209dbd7f6f39d07
dbaer2025 <dbaerfla@gmail.com>
2023-10-16 22:48:31 -0400

    hi

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "noteapp3": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "noteapp3f9743c61": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s340b4b9b9": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
